65f3d648c7fa56c30b85e9253d5ae088ec543410
daniel162738 <daniel.simorangkir.2402@gmail.com>
2024-03-17 10:06:01 +0700

    ...

---
+++ script/index.js
@@ -0,0 +1,12 @@
+
+window.onload = () => {
+    let header = document.querySelector('header');
+    header.classList.add('sticky');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+
+
